567cb7a1a5700d79ef8b3c77d3589648dfb17028
Yerlin Matu <yerlinmatu@gmail.com>
2018-04-14 18:15:56 -0500

    Structs or Interfaces is similar to object

---
+++ structs.go
@@ -0,0 +1,36 @@
+package main;
+
+import "fmt";
+
+type Person struct {
+	name string;
+	lastname string;
+	skill string;
+	age int;
+}
+
+func stripe() {
+	fmt.Println("----------------------------------");
+}
+
+func main() {
+	yerlin := Person { name: "Yerlin", lastname: "Matu", skill: "Software Developer", age: 20}
+	// also.
+	john := Person {"John", "Doe", "Baker", 32 }
+	
+	fmt.Println(yerlin);
+	fmt.Println(john);
+	stripe();
+
+	fmt.Println("Profile with details - Yerlin Matu");
+	fmt.Println("Name:", yerlin.name + yerlin.lastname);
+	fmt.Println("Skill:", yerlin.skill);
+	fmt.Println("Age:", yerlin.age);
+	stripe();
+
+	fmt.Println("Profile with details - John Doe");
+	fmt.Println("Name:", john.name + john.lastname);
+	fmt.Println("Skill:", john.skill);
+	fmt.Println("Age:", john.age);
+	stripe();
+}
