49b7001683309bda2408fde3400f08f2e8554aaa
Yerlin Matu <yerlinmatu@gmail.com>
2018-04-14 23:26:26 -0500

    Goroutines is to concurrent, asyn go similar a threads

--- goroutines.go
+++ goroutines.go
@@ -2,14 +2,20 @@ package main;
 
 import "fmt";
 import "strings";
+import "time";
 
 func main() {
-	nameSuperSlooow("Yerlinson");
+	go nameSuperSlooow("Yerlinson");
+	fmt.Println("It's boring to wait!");
+
+	var wait string;
+	fmt.Scanln(&wait);
 }
 
 func nameSuperSlooow(name string) {
 	letters := strings.Split(name, "");
 	for _,letter := range(letters) {
+		time.Sleep(1000 * time.Millisecond);
 		fmt.Println(letter);
 	}
 }
